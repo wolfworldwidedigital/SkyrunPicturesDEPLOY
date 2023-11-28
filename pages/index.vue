584b17960ac69b7e58b4d33d2fed92fc161fc48a
David | Wolf Worldwide <88748661+wolfworldwidedigital@users.noreply.github.com>
2023-11-28 21:17:10 +0530

    Add files via upload

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <IntroVideo v-if="showIntro" @videoEnded="closeIntro" />
+    <HomeVideo v-else />
+  </div>
+</template>
+
+<script setup>
+import IntroVideo from '~/components/Intros/IntroVideo.vue';
+import HomeVideo from '~/components/Intros/HomeVideo.vue';
+import { ref, onMounted } from 'vue';
+
+const showIntro = ref(true);
+
+onMounted(() => {
+  setTimeout(() => {
+    showIntro.value = false;
+  }, 20000); // 30 seconds
+});
+
+const closeIntro = () => {
+  showIntro.value = false;
+};
+
+</script>
